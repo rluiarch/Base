b14fda98c4f1d5f24b004be0086cba9dec943c64
Ray Lui <rluiarch@users.noreply.github.com>
2014-12-15 09:35:29 -0800

    Update set_repos.pp

--- manifests/set_repos.pp
+++ manifests/set_repos.pp
@@ -1,11 +1,6 @@
 class base::set_repos {
 
-    yumrepo { "RCS-Custom":
-      baseurl   => "http://54.193.1.156/RCS-Custom/",
-      descr   => "RCS Custom Repository",
-      enabled   => 1,
-      gpgcheck  => 0,
-    }
+
 
     yumrepo { "MongoDB":
       baseurl   => "http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/",
